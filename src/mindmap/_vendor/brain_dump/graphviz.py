ba79ddad962f4d5dd055933b7ad776b36bd28f91
RisingOrange <jakub.fidler@protonmail.com>
2021-02-22 12:09:52 +0100

    Change edge colors of bright theme

--- src/mindmap/_vendor/brain_dump/graphviz.py
+++ src/mindmap/_vendor/brain_dump/graphviz.py
@@ -4,23 +4,34 @@ from ..pydot import pydot
 from .parsers.indented_text_graph import parse as parse_text_graph
 
 
-
 class Theme:
-    DARKGREYBLUE = '#012b37'
-    # Palette from http://ethanschoonover.com/solarized
-    YELLOW = '#b58900'
-    ORANGE = '#cb4b16'
-    VIOLET = '#6c71c4'
-    RED = '#dc323f'
-    BLUE = '#268bd2'
-    MAGENTA = '#d33682'
-    CYAN = '#2aa198'
-    GREEN = '#859900'
-    GREY = '#939393'
+    SOLARIZED_BG_COLOR = '#012b37'
 
-    EDGE_COLORS = [YELLOW, ORANGE, VIOLET, RED, BLUE, MAGENTA, CYAN, GREEN, GREY]
+    BRIGHT_EDGE_COLORS = [ # Pallette from https://flatuicolors.com/palette/defo
+        '#f1c40f',
+        '#e67e22',
+        '#8e44ad',
+        '#e74c3c',
+        '#2980b9',
+        '#c0392b',
+        '#1abc9c',
+        '#27ae60',
+        '#95a5a6',
+    ]
 
-    def __init__(self, bg_color, label_color):
+    SOLARIZED_EDGE_COLORS = [  # Palette from http://ethanschoonover.com/solarized
+        '#b58900',
+        '#cb4b16',
+        '#6c71c4',
+        '#dc323f',
+        '#268bd2',
+        '#d33682',
+        '#2aa198',
+        '#859900',
+        '#939393',
+    ]
+
+    def __init__(self, bg_color, label_color, edge_colors):
         self.graph_style = dict(
             layout='twopi',
             overlap='false',
@@ -30,9 +41,10 @@ class Theme:
             outputorder="edgesfirst",
         )
         self.label_color = label_color
+        self.edge_colors = edge_colors
 
     def edge_style(self, dest_node, graph_height):
-        color = self.EDGE_COLORS[dest_node.branch_id % len(self.EDGE_COLORS)]
+        color = self.edge_colors[dest_node.branch_id % len(self.edge_colors)]
         return dict(
             color=color,
             dir='none',
@@ -40,40 +52,46 @@ class Theme:
         )
 
     def node_style(self, node, graph_height):
-        label = node.content.strip() if node.content and node.content != node.ROOT_DEFAULT_NAME else ''
+        label = node.content.strip(
+        ) if node.content and node.content != node.ROOT_DEFAULT_NAME else ''
         return dict(
             group=node.branch_id,
             shape='plaintext',
             label=label,
-            fontcolor=self.label_color ,
+            fontcolor=self.label_color,
             fontsize=2 * (16 + graph_height - node.depth),
-            fontname=self.graph_style['fontname'], # not inherited by default
+            fontname=self.graph_style['fontname'],  # not inherited by default
         )
 
     @classmethod
     def darksolarized(cls):
-        return cls(cls.DARKGREYBLUE, 'white')
+        return cls(cls.SOLARIZED_BG_COLOR, 'white', cls.SOLARIZED_EDGE_COLORS)
 
     @classmethod
     def bright(cls):
-        return cls('white', 'black')
+        return cls('white', 'black', cls.BRIGHT_EDGE_COLORS)
 
 
-def create_solarized_mindmap_img(input_filepath, output_file_path, theme=Theme.darksolarized(), root_label=None):
-    assert locale.getdefaultlocale()[1] == 'UTF-8' # needed to print 'Duplicate content' warning without error and to bypass pydot Dot.write default raw formatting on line 1769
+def create_solarized_mindmap_img(input_filepath, output_file_path, theme=Theme.bright(), root_label=None):
+    # needed to print 'Duplicate content' warning without error and to bypass pydot Dot.write default raw formatting on line 1769
+    assert locale.getdefaultlocale()[1] == 'UTF-8'
     with open(input_filepath) as txt_file:
         text = txt_file.read()
     graph = parse_text_graph(text, root_label=root_label)
     create_mindmap(graph, output_file_path, theme=theme)
 
+
 def create_mindmap(graph, output_svg_path, theme):
     graph_height = graph.height
     pygraph = pydot.Dot(root=graph.content, **theme.graph_style)
     for node in graph:
-        content = pydot.quote_if_necessary(node.content) # avoid erroneous pydot 'port' detection + workaround this: https://github.com/erocarrera/pydot/issues/187
-        pygraph.add_node(pydot.Node(content, **theme.node_style(node, graph_height)))
+        # avoid erroneous pydot 'port' detection + workaround this: https://github.com/erocarrera/pydot/issues/187
+        content = pydot.quote_if_necessary(node.content)
+        pygraph.add_node(pydot.Node(
+            content, **theme.node_style(node, graph_height)))
         if node.parent:
-            parent_content = node.parent.content if ':' not in node.parent.content else '"{}"'.format(node.parent.content)
-            pygraph.add_edge(pydot.Edge(parent_content, content, **theme.edge_style(node, graph_height)))
+            parent_content = node.parent.content if ':' not in node.parent.content else '"{}"'.format(
+                node.parent.content)
+            pygraph.add_edge(pydot.Edge(parent_content, content,
+                                        **theme.edge_style(node, graph_height)))
     pygraph.write_svg(output_svg_path, prog='twopi')
-
