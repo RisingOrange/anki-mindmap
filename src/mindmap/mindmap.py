bbd701837680125ff1bf6d919cb41255bcea7683
RisingOrange <jakub.fidler@protonmail.com>
2021-02-26 00:02:56 +0100

    Add DeckMindmap + Refactor

--- src/mindmap/mindmap.py
+++ src/mindmap/mindmap.py
@@ -1,4 +1,6 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from aqt import mw
 
 from ._vendor.brain_dump.graphviz import create_mindmap_img
 from .anki_util import get_notes, note_text
@@ -16,39 +18,80 @@ class Mindmap(ABC):
         with redirect_stderr_to_stdout():
             create_mindmap_img(tree_to_markdown(tree), output_file_path, theme)
 
+    @abstractmethod
+    def _paths(self):
+        pass
+
+    def _create_tree(self):
+        result = self._tree_from_paths()
+        result = self._add_note_texts_to_tree(result)
+        return result
+
+    def _tree_from_paths(self):
+        result = tree()
+
+        for path in self._paths():
+            if not path.startswith(self.prefix):
+                continue
+            self._traverse_tree(result, path)
+
+        return result
+
+    def _add_note_texts_to_tree(self, a_tree):
+        for path in self._paths():
+            for note in get_notes(f'"{self.query}:{path}*"'):
+                text = note_text(note, NOTE_TEXT_LENGTH_LIMIT)
+
+                if text is None:
+                    continue
+
+                self._traverse_tree(a_tree, path)[text] = tree()
+        return a_tree
+
+    def _traverse(self, path):
+        return self._traverse_tree(self, path)
+
+    def _traverse_tree(self, tree, path):
+        path_wh_prefix = self._without_prefix(path)
+        parts = path_wh_prefix.split(self.seperator)
+        cur = tree
+        for p in parts:
+            cur = cur[p]
+        return cur
+
+    def _without_prefix(self, name):
+        prefix_depth = len(self.prefix.split(self.seperator))
+        return self.seperator.join(name.split(self.seperator)[prefix_depth-1:])
+
 
 class TagMindmap(Mindmap):
 
     def __init__(self, tag_prefix):
-        self.tree = self._tag_tree(
-            tag_prefix, text_length_limit=NOTE_TEXT_LENGTH_LIMIT)
+        self.prefix = tag_prefix
+        self.query = 'tag'
+        self.seperator = cfg('tag_seperator')
+        self.tree = self._create_tree()
 
-    def _tag_tree(self, tag_prefix, root_name=None, text_length_limit=80):
-        result = tree()
+    def _paths(self):
+        return [
+            tag
+            for note in get_notes(f'"tag:{self.prefix}*"')
+            for tag in note.tags
+            if tag.startswith(self.prefix)
+        ]
 
-        notes = get_notes(f'"tag:{tag_prefix}*"')
-        for note in notes:
-            for tag in note.tags:
-                if not tag.startswith(tag_prefix):
-                    continue
 
-                prefix_tag_depth = len(tag_prefix.split(
-                    cfg('tag_seperator')))
-                tag_parts = tag.split(cfg('tag_seperator'))[
-                    prefix_tag_depth-1:]
-                cur = result
-                for p in tag_parts:
-                    cur = cur[p]
+class DeckMindmap(Mindmap):
 
-                text = note_text(note, text_length_limit)
-                if text is None:
-                    continue
-                cur[text] = tree()
+    def __init__(self, deck_prefix):
+        self.prefix = deck_prefix
+        self.query = 'deck'
+        self.seperator = '::'
+        self.tree = self._create_tree()
 
-        # add root node
-        if root_name is not None:
-            root = tree()
-            root[root_name] = result
-            result = root
-
-        return result
+    def _paths(self):
+        return [
+            deck_name
+            for deck_name in mw.col.decks.allNames()
+            if deck_name.startswith(self.prefix)
+        ]
